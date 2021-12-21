80082ca8dd87235c1590b3aa27f7a6c77e033b01
Imran <imran136@gmail.com>
2021-12-21 01:26:43 +0100

    Working app

--- ReferenceiOS/ViewController.swift
+++ ReferenceiOS/ViewController.swift
@@ -7,23 +7,79 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var label: UILabel! {
+    @IBOutlet weak var emailTextField: UITextField!{
         didSet {
-            label.accessibilityIdentifier = "label"
+            emailTextField.accessibilityIdentifier = "emailTextField"
         }
     }
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var passwordField: UITextField!{
+        didSet {
+            passwordField.accessibilityIdentifier = "passwordField"
+        }
+    }
+    @IBOutlet weak var errorText: UILabel!{
+        didSet {
+            errorText.accessibilityIdentifier = "errorText"
+            errorText.text=""
+        }
+    }
+    @IBOutlet weak var showMoney: UILabel! {
+        didSet {
+            showMoney.accessibilityIdentifier = "showMoney"
+        }
+    }
+    @IBOutlet weak var submitButton: UIButton!{
+        didSet {
+            submitButton.accessibilityIdentifier = "submitButton"
+        }
+    }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
     @IBAction func generate(_ sender: Any) {
-        // Generate amount
-        let amount = Float.random(in: 100 ..< 100000000)
+        //if email and password is valid then showing the generated amount
+        if isEmailValid(emailTextField.text ?? "") && isPasswordValid(passwordField.text ?? ""){
+            errorText.text=""
+            // Generate amount
+            let amount = Float.random(in: 100 ..< 100000000)
 
-        // Format
-        if let formattedAmount = CurrencyHelper.format(amount: amount as NSNumber) {
-            label.attributedText = CurrencyHelper.attributify(amount: formattedAmount)
+            // Format
+            if let formattedAmount = CurrencyHelper.format(amount: amount as NSNumber) {
+                showMoney.attributedText = CurrencyHelper.attributify(amount: formattedAmount)
+            }
+        }else{
+            errorText.text="Please provide valid credentials"
+            showMoney.text=""
+        }
+    }
+
+    
+    //checking if the email is valid or not
+    func isEmailValid(_ value: String) -> Bool {
+        do {
+            if try NSRegularExpression(pattern: "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$", options: .caseInsensitive).firstMatch(in: value, options: [], range: NSRange(location: 0, length: value.count)) == nil {
+                return false
+            }
+        } catch {
+            return false
+        }
+        return true
+    }
+    
+    //checking if the password legnth is smaller then 6 or not
+    func isPasswordValid(_ value: String) -> Bool {
+        if value.count<6 {
+            return false
+        } else {
+            return true
         }
     }
 }
