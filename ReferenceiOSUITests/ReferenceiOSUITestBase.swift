259405639d4c4252e3cf3260ff91b406d6c8eddc
Imran <imran136@gmail.com>
2021-12-21 02:07:14 +0100

    First working test

---
+++ ReferenceiOSUITests/ReferenceiOSUITestBase.swift
@@ -0,0 +1,25 @@
+//
+//  ReferenceiOSUITestBase.swift
+//  ReferenceiOSUITests
+//
+//  Created by Imran Hashim on 21/12/2021.
+//  Copyright © 2021 ABN AMRO. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class ReferenceiOSUITestBase: XCTestCase{
+    
+    let app = XCUIApplication()
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+}
--- ReferenceiOSUITests/ReferenceiOSUITests.swift
+++ ReferenceiOSUITests/ReferenceiOSUITests.swift
@@ -8,14 +8,14 @@
 
 import XCTest
 
-class ReferenceiOSUITests: XCTestCase {
+class ReferenceiOSUITests: ReferenceiOSUITestBase {
 
-    override func setUp() {
-        continueAfterFailure = false
-        XCUIApplication().launch()
-    }
-
-    func testGenerate() {
-//Test comment 
+    func testWithValidEmailValidPassword() {
+        //let app = XCUIApplication()
+        app/*@START_MENU_TOKEN@*/.textFields["emailTextField"]/*[[".textFields[\"Email\"]",".textFields[\"emailTextField\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.textFields["emailTextField"]/*[[".textFields[\"Email\"]",".textFields[\"emailTextField\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("avalidemail@valid.com")
+        app/*@START_MENU_TOKEN@*/.secureTextFields["passwordField"]/*[[".secureTextFields[\"Password\"]",".secureTextFields[\"passwordField\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.secureTextFields["passwordField"]/*[[".secureTextFields[\"Password\"]",".secureTextFields[\"passwordField\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("1234qwer")
+        app/*@START_MENU_TOKEN@*/.staticTexts["Submit"]/*[[".buttons[\"Submit\"].staticTexts[\"Submit\"]",".buttons[\"submitButton\"].staticTexts[\"Submit\"]",".staticTexts[\"Submit\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
     }
 }
